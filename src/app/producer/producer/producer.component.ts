12df8eb26e731849abc4a646ac4479594d345504
Samad Sajanlal <samad.sajanlal@gmail.com>
2018-08-05 17:53:25 -0500

    update 3

--- src/app/producer/producer/producer.component.ts
+++ src/app/producer/producer/producer.component.ts
@@ -2,46 +2,46 @@ import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {EosService} from '../../services/eos.service';
 import {Subscription} from 'rxjs';
-import {TransactionService} from '../../services/transaction.service';
+import {ProducerService} from '../../services/producer.service';
 
 @Component({
-  selector: 'app-transaction',
-  templateUrl: './transaction.component.html'
+  selector: 'app-producer',
+  templateUrl: './producer.component.html'
 })
-export class TransactionComponent implements OnInit {
+export class ProducerComponent implements OnInit {
   public id: string;
-  public transaction = null;
+  public producer = null;
   public actions = null;
-  public transactionRaw = null;
+  public producerRaw = null;
   private subscriber: Subscription;
   page = 1;
 
   constructor(
     private route: ActivatedRoute,
     private eosService: EosService,
-    private transactionService: TransactionService
+    private producerService: ProducerService
   ) { }
 
   ngOnInit() {
     this.id = this.route.snapshot.params['id'];
 
-    this.transactionService.getTransaction(this.id).subscribe(data => {
-      this.transaction = data;
-      console.log(this.transaction);
+    this.producerService.getProducer(this.id).subscribe(data => {
+      this.producer = data;
+      console.log(this.producer);
 
       this.subscriber = this.route.queryParams.subscribe(params => {
         this.page = params['page'] || 1;
-        
-        this.transactionService.getTransactionActions(this.id, this.page).subscribe(data => {
+
+        this.producerService.getProducerActions(this.id, this.page).subscribe(data => {
           this.actions = data;
           console.log(data);
         });
       });
 
-      this.eosService.eos.getBlock(this.transaction.blockId).then(result => {
-        for (let index in result.transactions) {
-          if (result.transactions[index].trx.id == this.transaction.id) {
-            this.transactionRaw = result.transactions[index];
+      this.eosService.eos.getBlock(this.producer.blockId).then(result => {
+        for (let index in result.producers) {
+          if (result.producers[index].trx.id == this.producer.id) {
+            this.producerRaw = result.producers[index];
             return;
           }
         }
